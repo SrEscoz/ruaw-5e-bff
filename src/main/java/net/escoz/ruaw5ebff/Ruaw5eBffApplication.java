7f9ce93ac58839d603d16b3d3045b66f68c07494
Escoz <kyrovilches@gmail.com>
2024-11-24 13:01:16 +0100

    Commit inicial

---
+++ src/main/java/net/escoz/ruaw5ebff/Ruaw5eBffApplication.java
@@ -0,0 +1,13 @@
+package net.escoz.ruaw5ebff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ruaw5eBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ruaw5eBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/escoz/ruaw5ebff/Ruaw5eBffApplicationTests.java
@@ -0,0 +1,13 @@
+package net.escoz.ruaw5ebff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ruaw5eBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
