0edac2b2673af0dd1933401cc43588cad45e7c56
Escoz <kyrovilches@gmail.com>
2024-12-12 21:42:31 +0100

    Validaciones (se me había pasado)

--- src/main/java/net/escoz/ruaw5ebff/controllers/dtos/user/UserLoginDTO.java
+++ src/main/java/net/escoz/ruaw5ebff/controllers/dtos/user/UserLoginDTO.java
@@ -1,5 +1,7 @@
 package net.escoz.ruaw5ebff.controllers.dtos.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public final class UserLoginDTO {
 
+	@NotBlank(message = "El email es obligatorio")
 	private String email;
+
+	@NotBlank(message = "El correo es obligatorio")
+	@Email(message = "Formato de correo inválido")
 	private String password;
 }
